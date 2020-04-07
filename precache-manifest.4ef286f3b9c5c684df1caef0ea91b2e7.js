6b8d86ee8efb00a9c04d45f0baa33722bdd1e4f2
poliva <poliva@llnw.com>
2020-04-07 12:06:15 -0400

    Updates

---
+++ precache-manifest.4ef286f3b9c5c684df1caef0ea91b2e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b61b55ca883bbec9af7125421ac8d95",
+    "url": "/covid19stimulus/index.html"
+  },
+  {
+    "revision": "e125457a747ddc96de9d",
+    "url": "/covid19stimulus/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "334f63c02484bcc540a6",
+    "url": "/covid19stimulus/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "e125457a747ddc96de9d",
+    "url": "/covid19stimulus/static/js/2.720ab00b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid19stimulus/static/js/2.720ab00b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "334f63c02484bcc540a6",
+    "url": "/covid19stimulus/static/js/main.5cbd1ae1.chunk.js"
+  },
+  {
+    "revision": "e9fd97c1f7671b396681",
+    "url": "/covid19stimulus/static/js/runtime-main.58b35399.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19stimulus/static/media/logo.5d5d9eef.svg"
+  }
+]);
